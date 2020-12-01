668967cdf219bbe98cee7237872d043cf754d70f
Artem Kiselev <akiselev.prod@gmail.com>
2020-12-01 20:58:31 +0300

    Add navigation
    Now i have initial navigation, few templates and css.

--- lazyhelper/urls.py
+++ lazyhelper/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('catalog', views.catalog, name='catalog')
+    path('', views.index, name='home'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('user/<str:pk>/', views.user, name='user'),
+    path('product/<str:pk>/', views.product, name='product'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('guarantees/', views.guarantees, name='guarantees')
 ]
--- lazyhelper/views.py
+++ lazyhelper/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return render(request, 'lazyhelper/index.html')
+    products = Product.objects.all()[:3]
+    return render(request, 'lazyhelper/home.html', {'products': products})
 
 def catalog(request):
     return render(request, 'lazyhelper/catalog.html')
+
+def user(request, pk):
+    user = User.objects.get(id=pk)
+    orders = user.customer.all()
+    if user.role.role == 'Customer':
+        orders = user.customer.all()    
+    elif user.role.role == 'Contractor':
+        orders = user.contractor.all()
+    order_count = orders.count()
+    context = {'user': user, 'orders': orders, 'order_count': order_count}
+    return render(request, 'lazyhelper/user.html', context)
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'lazyhelper/product.html')
+
+def feedback(request):
+    return render(request, 'lazyhelper/feedback.html')
+
+def guarantees(request):
+    return render(request, 'lazyhelper/guarantees.html')
